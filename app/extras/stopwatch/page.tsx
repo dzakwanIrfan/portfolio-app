255dd8aa3c93254ca21fd57f3178dd0a8579a79f
Dzakwan Irfan <lexdani368@gmail.com>
2024-02-01 22:39:01 +0700

    trial and error 1

--- app/extras/stopwatch/page.tsx
+++ app/extras/stopwatch/page.tsx
@@ -1,6 +1,37 @@
+'use client'
+import React, { useState, useEffect } from 'react';
 import './style.css'
 
 export default function Stopwatch() {
+    const [ms, setMs] = React.useState(0);
+    const [s, setS] = React.useState(0);
+    const [isRunning, setIsRunning] = React.useState(false);
+    const intervalRef = React.useRef();
+
+    const handleStart = () => {
+        setIsRunning(!isRunning);
+    }
+
+    useEffect(() => {
+        if (isRunning) {
+            intervalRef.current = setInterval(() => {
+                setMs((prevMs) => {
+                    if (prevMs === 99) {
+                        setS((prevS) => prevS + 0.5);
+                        return 0;
+                    }
+                    return prevMs + 1;
+                });
+            }, 1);
+        } else {
+            clearInterval(intervalRef.current);
+        }
+
+        return () => {
+            clearInterval(intervalRef.current);
+        };
+    }, [isRunning]);
+
     return (
         <div className="container">
             <div id="time">
@@ -13,16 +44,16 @@ export default function Stopwatch() {
                     <p>M</p>
                 </span>
                 <span className='wrap'>
-                    <div className="time" id="second">00</div>    
+                    <div className="time" id="second">{s}</div>    
                     <p>S</p>
                 </span>
                 <span className='wrap'>
-                    <div className="time" id="milisecond">00</div>    
+                    <div className="time" id="milisecond">{ms}</div>    
                     <p>Ms</p>
                 </span>
             </div>
             <div id="buttons">
-                <button className='button'>Start</button>
+                <button className='button' onClick={handleStart}>{isRunning ? 'Pause' : 'Start'}</button>
                 <button className='button'>Split</button>
                 <button className='button'>Reset</button>
             </div>

9973aba8df19293e59ceffa9b72204fac70a5a22
Dzakwan Irfan <lexdani368@gmail.com>
2024-02-03 18:11:53 +0700

    fitur start stopwatch

--- app/extras/stopwatch/page.tsx
+++ app/extras/stopwatch/page.tsx
@@ -1,63 +1,52 @@
 'use client'
-import React, { useState, useEffect } from 'react';
-import './style.css'
+
+import React, { useState, useEffect, useRef } from 'react';
+import './style.css';
 
 export default function Stopwatch() {
-    const [ms, setMs] = React.useState(0);
-    const [s, setS] = React.useState(0);
-    const [isRunning, setIsRunning] = React.useState(false);
-    const intervalRef = React.useRef();
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [now, setNow] = useState<number | null>(null);
+  const intervalRef = useRef<number | null>(null);
 
-    const handleStart = () => {
-        setIsRunning(!isRunning);
-    }
+  function handleStart() {
+    setStartTime(Date.now());
+    setNow(Date.now());
 
-    useEffect(() => {
-        if (isRunning) {
-            intervalRef.current = setInterval(() => {
-                setMs((prevMs) => {
-                    if (prevMs === 99) {
-                        setS((prevS) => prevS + 0.5);
-                        return 0;
-                    }
-                    return prevMs + 1;
-                });
-            }, 1);
-        } else {
-            clearInterval(intervalRef.current);
-        }
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  }
+  
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
 
-        return () => {
-            clearInterval(intervalRef.current);
-        };
-    }, [isRunning]);
-
-    return (
-        <div className="container">
-            <div id="time">
-                <span className='wrap'>
-                    <div className="time" id="hour">00</div>    
-                    <p>H</p>
-                </span>
-                <span className='wrap'>
-                    <div className="time" id="minute">00</div>    
-                    <p>M</p>
-                </span>
-                <span className='wrap'>
-                    <div className="time" id="second">{s}</div>    
-                    <p>S</p>
-                </span>
-                <span className='wrap'>
-                    <div className="time" id="milisecond">{ms}</div>    
-                    <p>Ms</p>
-                </span>
-            </div>
-            <div id="buttons">
-                <button className='button' onClick={handleStart}>{isRunning ? 'Pause' : 'Start'}</button>
-                <button className='button'>Split</button>
-                <button className='button'>Reset</button>
-            </div>
-            
-        </div>
-    );
+  return (
+    <div className="container">
+      <div id="milisecond">{secondsPassed.toFixed(3)}</div>
+      <div id="time">
+        <span className="wrap">
+          <div className="time" id="hour">00</div>
+          <p>H</p>
+        </span>
+        <span className="wrap">
+          <div className="time" id="minute">00</div>
+          <p>M</p>
+        </span>
+        <span className="wrap">
+          <div className="time" id="second">00</div>
+          <p>S</p>
+        </span>
+      </div>
+      <div id="buttons">
+        <button className="button" onClick={handleStart}>
+            Start
+        </button>
+        <button className="button">Split</button>
+        <button className="button">Reset</button>
+      </div>
+    </div>
+  );
 }
